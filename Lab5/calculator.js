60b42271371b23e23d6720a511672bfc9b617e8d
CHijYh <cod22batlemt@gmail.com>
2025-10-12 15:10:02 +0300

    Update calculator.js

--- Lab5/calculator.js
+++ Lab5/calculator.js
@@ -1,27 +1,22 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const calculateBtn = document.getElementById('calculate');
-  const quantityInput = document.getElementById('quantity');
-  const productSelect = document.getElementById('product');
-  const message = document.getElementById('message');
+document.addEventListener("DOMContentLoaded", function () {
+    var calculateButton = document.getElementById("calculate");
+    var quantityInput = document.getElementById("quantity");
+    var productSelect = document.getElementById("product");
+    var message = document.getElementById("message");
 
-  calculateBtn.addEventListener('click', function () {
-    const quantityStr = quantityInput.value.trim();
+    calculateButton.addEventListener("click", function () {
+        var quantityStr = quantityInput.value.trim();
+        var isValid = /^[1-9]\d*$/.test(quantityStr);
+        var quantity = parseInt(quantityStr);
+        var price = parseInt(productSelect.value);
+        var total = quantity * price;
+        if (!isValid) {
+            message.style.color = "Red";
+            message.textContent = "Пожалуйста, введите корректное количество.";
+            return;
+        }
 
-    const isValid = /^[1-9]\d*$/.test(quantityStr);
-
-    if (!isValid) {
-      message.style.color = 'Red';
-      message.textContent = 'Пожалуйста, введите корректное количество (только положительные целые числа).'; 
-      return;
-    }
-
-    message.style.color = 'Black';
-
-    const quantity = parseInt(quantityStr);
-    const price = parseInt(productSelect.value);
-
-    const total = quantity * price;
-
-    message.textContent = 'Общая стоимость заказа: '+ total +' руб.';
-  });
+        message.style.color = "Black";
+        message.textContent = "Общая стоимость заказа: " + total + " руб.";
+    });
 });
